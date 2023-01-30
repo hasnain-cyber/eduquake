d332b2f1b86ae50a4a2707798b5b3f3ec6c323ca
Hasnain Raza <hasnainraza1072@gmail.com>
2023-01-30 23:31:29 +0530

    fix update and delete bugs with courseModel controllers

--- server/controllers/courseController.ts
+++ server/controllers/courseController.ts
@@ -17,26 +17,22 @@ const createCourse = async (req: Request, res: Response) => {
 }
 
 const getCourseById = async (req: Request, res: Response) => {
-    const course = await courseModel.findById(req.params.id);
+    const course = await courseModel.query('id').eq(req.params.id).exec();
     res.status(200).json(course);
 }
 
 const updateCourse = async (req: Request, res: Response) => {
-    const course = await courseModel.findById(req.params.id);
-    if (course) {
-    } else {
-        res.status(404).json({ message: 'Course not found' });
-    }
+    const courseId = req.params.id;
+    const course = await courseModel.update({
+        id: courseId,
+        ...req.body
+    });
+    res.status(200).json(course);
 }
 
 const deleteCourse = async (req: Request, res: Response) => {
-    const course = await courseModel.findById(req.params.id);
-    if (course) {
-        await course.remove();
-        res.status(200).json({ message: 'Course removed' });
-    } else {
-        res.status(404).json({ message: 'Course not found' });
-    }
+    const course = await courseModel.delete(req.params.id);
+    res.status(200).json(course);
 }
 
 export default {
